77a7912861753250b144f5d4ba002cf8f5c82989
Dmitry Pavlenko <125051658+DPavlen@users.noreply.github.com>
2024-10-03 19:33:44 +0300

    Add app cats: models Group, Breed, Cat & admin all models. Add settings & pyproject: django-colorfield.

--- backend/cats/admin.py
+++ backend/cats/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Group, Breed, Cat
+
+
+class BaseAdminSettings(admin.ModelAdmin):
+    """Базовые настройки для всех моделей."""
+    list_display = ("id", "name", "created")
+    list_display_links = ("id", "name", "created")
+    search_fields = ("id", "name", "created")
+    empty_value_display = "-пусто-"
+
+
+@admin.register(Group)
+class GroupAdmin(BaseAdminSettings):
+    """Административный интерфейс для управления группами котов."""
+    pass
+
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    """Административный интерфейс для управления породами котов."""
+    list_display = BaseAdminSettings.list_display + ("group",)
+    list_display_links = BaseAdminSettings.list_display_links + ("group",)
+    search_fields = BaseAdminSettings.search_fields + ("group",)
+
+
+@admin.register(Cat)
+class CatAdmin(BaseAdminSettings):
+    """Административный интерфейс для управления породами котов."""
+    list_display = BaseAdminSettings.list_display + (
+        "breed", "color", "age", "sex")
+    list_display_links = BaseAdminSettings.list_display_links + (
+        "breed", "color", "age", "sex")
+    search_fields = BaseAdminSettings.search_fields + (
+        "breed", "color", "age", "sex")
--- backend/cats/models.py
+++ backend/cats/models.py
@@ -1,3 +1,118 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from colorfield.fields import ColorField
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class Group(TimeStampedModel):
+    """Модель группы котов."""
+    id = models.UUIDField(
+        "id",
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name = models.CharField(
+        "Название группы котов",
+        max_length=100,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Группа кота"
+        verbose_name_plural = "Группы котов"
+        ordering = ("created",)
+
+    def __str__(self):
+        return self.name
+
+
+class Breed(TimeStampedModel):
+    """Модель породы котов."""
+    id = models.UUIDField(
+        "id",
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name = models.CharField(
+        "Название породы кота",
+        max_length=100,
+        unique=True,
+    )
+    group = models.ForeignKey(
+        Group,
+        verbose_name="Группа котов",
+        on_delete=models.CASCADE,
+        related_name="breeds"
+    )
+
+    class Meta:
+        verbose_name = "Порода кота"
+        verbose_name_plural = "Породы котов"
+        ordering = ("created",)
+
+    def __str__(self):
+        return self.name
+
+
+class Cat(TimeStampedModel):
+    """Модель котов."""
+    SEX_CHOICES = (
+        ("Tomcat", "Кот"),
+        ("Queen", "Кошка")
+    )
+    id = models.UUIDField(
+        "id",
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name = models.CharField(
+        "Имя кота",
+        max_length=50,
+    )
+    breed = models.ForeignKey(
+        Breed,
+        verbose_name="Порода кота",
+        on_delete=models.CASCADE,
+        related_name="cats",
+    )
+    color = ColorField(
+        verbose_name="Цвет в формате HEX",
+        max_length=7,
+        format="hex",
+        default="#808080",
+        unique=True,
+        # validators=[ColorValidator],
+    )
+    age = models.PositiveIntegerField(
+        "Возраст кота",
+        null=True,
+        blank=True,
+    )
+    sex = models.CharField(
+        max_length=10,
+        verbose_name="Пол кота",
+        choices=SEX_CHOICES,
+        null=True,
+        blank=True,
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Текст описания кота"
+    )
+
+    class Meta:
+        verbose_name = "Кот"
+        verbose_name_plural = "Коты"
+        ordering = ("created",)
+
+    def __str__(self):
+        return self.name
+
